d1e5ceaaf592844e642ee83412dd690dc7ae7138
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-04-20 23:00:44 +0400

    5s6df6d4sfs

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,8 @@
  */
 int main(void)
 {
-	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 1, 59, stderr);
+	message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	fwrite(message, 1, 59, stderr);
 	return (1);
 }
