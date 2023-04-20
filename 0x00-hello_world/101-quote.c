8f59569e6aba583e447a67415876aa8e7ba4f092
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-04-20 22:50:21 +0400

    dss6f5sd6

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 #include <string.h>
 
 /**
- * main - C program to printuwing a quote
- * Return : 1
+ * main - C program to printing a quote
+ * Return : 1 (Success)
  */
 int main(void)
 {
-        fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 1, 59, stderr);
-        return (1);
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 1, 59, stderr);
+	return (1);
 }
