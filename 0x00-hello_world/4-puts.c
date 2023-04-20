a3f5c6dd928ca1018385b1df2a5e0d77be95b43a
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-04-20 19:17:11 +0400

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - writing a program to print a line with put function
+ * Return: 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
