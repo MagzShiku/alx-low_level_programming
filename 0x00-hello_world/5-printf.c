81a0df48ef132ee5e0be096baf61b6d993cedddb
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-04-20 19:38:31 +0400

    task5%2

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main - writing a program that reads exactly with proper grammar, but the outcome is a piece of art, followed by a new line \n
- *
- * Return: 0  (for success)
+ * main - writing a program that reads exactly line
+ * Return: 0
  */
 int main(void)
 {
-	printf ("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
