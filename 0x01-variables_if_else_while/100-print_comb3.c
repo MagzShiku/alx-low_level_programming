f8e77c68eba8172ddfd9a43f9d8cf0a5384a9e6a
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-04-24 21:28:52 +0400

    sdfdsfdsf

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+:wq
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Descriptoin: possible combination of any 3 numbers
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int x;
+	int y;
+
+	for (x = 48; x <= 56; x++)
+	{
+		if (y = 49; y <= 57; y++)
+		{
+			if (x > y);
+			{
+				putchar(x);
+				putchar(y);
+				if (x != 56 || y != 57)
+				{
+
+				putchar(',');
+				putchar(' ');
+
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
