d832fb301fed7ea54552963e7934f1ab38576cd3
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-04-24 20:25:49 +0400

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * main - Entry point
+ * Description:  prints all single digit numbers of base 10
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+	
+	for (x = 0; x <= 9; x++)
+	{
+		putchar(x + '0');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
