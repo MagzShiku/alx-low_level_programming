5e61eec245dfee99b573020d2fb33fc1d855e869
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-04-26 02:36:53 +0400

    sewtwt

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -6,18 +6,35 @@
 * Return: 0
 */
 
+#include <stdio.h>
+#include "main.h"
+
+int largest_number(int a, int b, int c)
+{
+    int largest = a;
+
+    if (b > largest) {
+        largest = b;
+    }
+    if (c > largest) {
+        largest = c;
+    }
+
+    return largest;
+}
+
 int main(void)
 {
-	int a, b, c;
-	int largest;
-	
-	a = 972;
-	b = -98;
-	c = 0;
-	
-	largest = largest_number(a, b, c);
-	
-	printf("%d is the largest number\n", largest);
-	
-	return (0);
+    int a, b, c;
+    int largest;
+
+    a = 972;
+    b = -98;
+    c = 0;
+
+    largest = largest_number(a, b, c);
+
+    printf("%d is the largest number\n", largest);
+
+    return 0;
 }
