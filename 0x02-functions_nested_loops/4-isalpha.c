e60114462df93c7d822bb546e0d9cab1fcd4fb14
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-04-25 04:54:37 +0400

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - function to check  for alphabetic character
+ *
+ * @c: Charactert to be checked for lower, upper ir existing
+ * Return: 1 if found, 0 if other
+ */
+
+int _isalpha(int c)
+{
+if ((c >= 65 && c <= 97) || (c >= 97 && c <= 122))
+{
+return (1);
+}
+return (0);
+}
