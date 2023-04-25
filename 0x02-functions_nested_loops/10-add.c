5b1e77bcc4fd367da368f4c909b00eee2ddd7976
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-04-25 08:15:36 +0400

    simple sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds to integers
+ * @a: first parameter
+ * @b: second parameter
+ * Return: returns sum
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+	_putchar('\n');
+}
