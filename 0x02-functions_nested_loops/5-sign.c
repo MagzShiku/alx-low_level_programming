9c05b44543400f00d7f5473bf635b198f18a7c87
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-04-25 05:54:34 +0400

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,9 +3,7 @@
 /**
  * print_sign - prints the sign of a number
  * @n: character to use if the number is greater than 0
- * Return: 1 if number is greater than 0
- * Return: 0 if number is 0
- * Return: -1 if number is less than 1
+ * Return: 1 if n is > than 0, 0 if n is = 0 & -1 for n < 0
  */
 
 int print_sign(int n)
@@ -18,10 +16,10 @@ int print_sign(int n)
 	else if (n < 0)
 	{
 		_putchar(45);
-		return(-1);
+		return (-1);
 	}
 	else
-	{	
+	{
 		_putchar(48);
 		return (0);
 	}
