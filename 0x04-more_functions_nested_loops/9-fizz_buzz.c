d440615328bc6c02da8cf79049601f3ce50affaf
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-04-27 22:25:19 +0400

    dfs

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: this prints Fizz, Buzz & FizzBuzz depending on the modulus
+ * Return: 0 for Success
+ */
+
+int main(void)
+{
+
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if (1 % 10 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else
+			printf("%i", i);
+		if (i < 100)
+			printf(" ");
+
+	}
+printf("\n");
+return (0);
+}
