6213f758e75ec29b3a422e80d9b5c85344b12a04
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-04-27 15:22:50 +0400

    main_c_files

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - checking for upper case character
+ *
+ * Return: 1 if c is upper case
+ * Return: 0 if otherwise
+ */
+
+int _isupper(int c)
+{
+	for (c = 65; c <= 90; c++)
+		       _putchar("%\d" ':');
+			_putchar('\n');
+			return (1);
+}
