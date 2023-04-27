fa719f68c65f7b960e8655eb4617c4eabc0c3f89
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-04-28 01:46:22 +0400

    README.me redo

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,6 +1,6 @@
-:wq
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
 
 /**
  * main - Entry point
--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,47 +1,49 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * print_times_table - print the n times table
- * @n: the integer for n
- * Return: Always void (Success)
+ * print_times_table - prints the n times table, starting with 0
+ *
+ * @n: number of the times table
  */
 void print_times_table(int n)
 {
-	int a, b, c;
+int i, j, k;
 
-	if (n > 15 || n < 0)
-		return;
-
-	for (a = 0; a <= n; a++)
-	{
-	for (b = 0; b <= n; b++)
-	{
-	c = a * b;
-	if (b != 0)
-	{
-		_putchar(',');
-		_putchar(' ');
-	if (c < 100)
-	{
-		_putchar(' ');
-	}
-	if (c < 10)
-	{
-		_putchar(' ');
-	}
-	}
-	if (c >= 100)
-	{
-		_putchar((c / 100) + '0');
-		_putchar(((c / 10)) % 10 + '0');
-	}
-	else if (c >= 10)
-	{
-		_putchar(' ');
-		_putchar((c / 10) + '0');
-	}
-	_putchar((c % 10) + '0');
-	}
-	_putchar('\n');
-	}
+if (n >= 0 && n <= 15)
+{
+for (i = 0; i <= n; i++)
+for (j = 0; j <= n; j++)
+{
+k = j * i;
+if (j == 0)
+{
+_putchar(k + '0');
+}
+else if (k < 10 && j != 0)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(' ');
+_putchar(k + '0');
+}
+else if (k >= 10 && k < 100)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar((k / 10) + '0');
+_putchar((k % 10) + '0');
+}
+else if (k >= 100)
+{
+_putchar(',');
+_putchar(' ');
+_putchar((k / 100) + '0');
+_putchar(((k / 10) % 10) + '0');
+_putchar((k % 10) + '0');
+}
+}
+_putchar('\n');
+}
 }
