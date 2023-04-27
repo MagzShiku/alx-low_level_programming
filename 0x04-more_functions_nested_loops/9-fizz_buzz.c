871cbcfcdd8265260e74e6e83a137e22655bf97c
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-04-27 22:32:46 +0400

    dfgf

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,18 +14,18 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
-		{
-			printf("Fizz");
-		}
-		else if (i % 5 == 0)
-		{
-			printf("Buzz");
-		}
-		else if (1 % 10 == 0)
-		{
+		if (i % 15 == 0)
+		
 			printf("FizzBuzz");
-		}
+		
+		else if (i % 3 == 0)
+		
+			printf("Fizz");
+		
+		else if (i % 3 == 0)
+		
+			printf("Buzz");
+		
 		else
 			printf("%i", i);
 		if (i < 100)
