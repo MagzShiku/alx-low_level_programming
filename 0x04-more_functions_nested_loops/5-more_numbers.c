b1a5319dd80fb53e174ba37f3282f784a365131a
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-04-27 17:40:14 +0400

    why to use %? Why use null?

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * more_numbers - function name
+ *
+ * Return: 0 for success
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+			putchar('1');
+			putchar(j % 10 + '0');
+		}
+		putchar('\n');
+	}
+}
