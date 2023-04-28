031e4f7835916e916ec952a18f9ccda5c6581246
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-04-28 04:30:12 +0400

    check for digit 0-9(i have used ANCII)

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - function name. checks for a digit 0-9
+ * @c: parameter to be checked
+ * Return: 1 if yes, 0 if no
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
