67be8b2278d2f511076ad49d4aef277f67d1a4c8
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-04-28 04:45:20 +0400

    return product

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - function name to represent a produc tof 2 integers
+ * @a: integer 1
+ * @b: integre 2
+ * Return: 0 Always for success
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
