6733f5582051a909417da7e0136ae2d4be55b3d1
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-04-28 11:05:02 +0400

    more_numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,22 +10,15 @@ void more_numbers(void)
 	int i;
 	int a;
 
-	i = 0;
-
-	while (i < 10)
+	for (i = 0; i <= 10; i++)
 	{
-		i++;
-
-		for (a = 48; a <= 57; a++)
+		for (a = 0; a <= 14; a++)
 		{
-			_putchar(a);
-		}
-		for (a = 10; a <= 14; a++)
-		{
+			if (a >= 10)
 			_putchar('1');
 			_putchar(a % 10 + '0');
 		}
-	_putchar ('\n');
+	_putchar('\n');
 
 	}
 
