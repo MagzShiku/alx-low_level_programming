fe885f4f90093cd999f3ec05f977393af1057df2
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-05-02 18:35:04 +0400

    Battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reset_to_98 - takes a pointer to int and assigns it 98
+ * @n: the pointer that needs to be asigned 98
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
