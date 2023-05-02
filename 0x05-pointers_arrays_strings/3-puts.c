469e359502734fce9a030e3547baf3e8cee6e60f
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-05-02 19:23:13 +0400

    print string using _putchar

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _puts - function name to print string
+ * @str: the string to be strung
+ * Return: String
+ */
+
+void _puts(char *str)
+{
+	_putchar(puts(str));
+
+}
