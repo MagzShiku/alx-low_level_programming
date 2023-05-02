0575985c07eb848619cb750fc077dcefca47aa1c
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-05-02 21:56:44 +0400

    needs redo

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of arrays and integers
+ * @a: parameter 1
+ * @n: parameter 2
+ * Return: Always 0
+ */
+
+void print_array(int *a, int n)
+{
+	int arr;
+
+	for (arr = 0; arr < n; arr++)
+	{
+		printf("%d", a[arr]);
+
+		if (arr != (n - 1))
+		{
+			printf(", ");
+		}
+	}
+
+printf("\n");
+}
