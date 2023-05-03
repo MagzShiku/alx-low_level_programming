40811f8ff39d230de4237dfb68994f1194fd42ec
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-05-03 13:52:23 +0400

    needs to undestand

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -14,26 +14,41 @@ int _atoi(char *s)
 	int a;
 	int b;
 	int c;
+	int d_length;
+	int e;
+	int f_num;
 
-	a = 1;
+	a = 0;
 	b = 0;
 	c = 0;
+	d_length = 0;
+	e = 0;
+	f_num = 0;
 
-	while (s[c] != '\0')
+	while (s[d_length] != '\0')
+		d_length++;
+
+	while (a < d_length && e == 0)
 	{
-		if (s[c] == '-')
+		if (s[a] == '-')
+			++b;
+
+		if (s[a] >= '0' && s[a] <= '9')
 		{
-			a = -1;
+			f_num = s[a] - '0';
+			if (b % 2)
+				f_num = -f_num;
+
+			c = c * 10 + f_num;
+			e = 1;
+
+			if (s[a + 1] < '0' || s[a + 1] > '9')
+				break;
+			e = 0;
 		}
-		else if (s[c] >= '0' && s[c] <= '9')
-		{
-			b = b * 10 + (s[c] - '0');
-		}
-		else if (b > 0)
-		{
-			break;
-		}
-		c++;
+		a++;
 	}
-return (a * b);
+if (e == 0)
+	return (0);
+return (c);
 }
