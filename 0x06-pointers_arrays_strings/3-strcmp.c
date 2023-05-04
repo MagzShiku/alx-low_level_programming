f52bfc2fd852c76d8a35bbc47a97a38a92abed2e
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-05-04 19:05:16 +0400

    my strings compared

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp -  comparing two strings
+ * @s1: Parameter 2
+ * @s2:  Parameter 3
+ * Return: Always 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int result;
+
+	result = strcmp(s1, s2);
+	return (result);
+}
