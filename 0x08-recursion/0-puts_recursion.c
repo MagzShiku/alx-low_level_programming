2f2f9e877912a48bed3d0f61270695a385a1ff69
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-05-10 15:49:01 +0400

    to print each character in a string in recursive

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _puts_recursion - to print a string followed by a new line
+ * @s: the string to be printed
+ * Return: Always 0
+ */
+
+void _puts_recursion(char *s) /*the prototype*/
+{
+	if (*s == '\0')
+	{
+		return;
+		printf("\n");
+	}
+	else
+	{
+	_putchar(*s);
+	_puts_recursion(++s);
+	_putchar('\n');
+	}
+
+}
+			     
