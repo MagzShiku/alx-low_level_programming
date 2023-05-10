096ec341dc5f2a7fd5cc48b988f65330c3be95b7
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-05-10 22:58:40 +0400

    this is the best I can push fot today in squarre

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <math.h>
+
+i
+/**
+ * _sqrt_recursion -  a function that returns the natural square root of a number.
+ * @n: number whose square root i]has been printed
+ * Return: -1 if  n does not have a square root
+ */
+
+int _sqrt_recursion(int n)
+{
+	int _sqrt_recursion = 
+	return (sqrt(n, 1));
+
+}
