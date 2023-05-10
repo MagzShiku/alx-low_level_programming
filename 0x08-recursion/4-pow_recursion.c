7bd252e348335ad22b42688c0b57266dc5f73cae
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-05-10 17:32:25 +0400

    the power of pow woooooooow

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * _pow_recursion - function that returns the value of x raised to the power of y
+ * @x: value 1
+ * @y: the power raise
+ * Return:  -1 if y is lower than 0
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+
+	}
+
+	else if (y == 0)
+	{
+		return (1);	
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+
+}
