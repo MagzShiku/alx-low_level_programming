463de56c4d09df93e1ce2956d84e362f61f3bac2
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-05-16 10:12:09 +0400

    creates an array of chars, and initializes it with a specific char hot fix2

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 
 /**
@@ -11,22 +12,30 @@
 
 char *create_array(unsigned int size, char c)
 {
+	char *str;
+	unsigned int i;
+
 	if (size == 0)
-	
+	{
 		return (NULL);
-	
 
-	char *array = (char *)malloc(size * sizeof(char));
-	if (array == NULL)
-	
+	}
+
+	str = (char *)malloc(size * sizeof(char));
+
+	if (str == NULL)
+	{
 		return (NULL);
-	
 
-	for (unsigned int i = 0; i < size; i++)
-	
-		array[i] = c;
-	
+	}
 
-	return (array);
-	
+	i = 0;
+	while (i < size)
+	{
+		str[i] = c;
+		i++;
+
+	}
+	return (str);
+
 }
