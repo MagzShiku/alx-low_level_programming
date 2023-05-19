05e6a40b76a458f5c30c642e1e8f65027f702858
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-05-19 14:43:42 +0400

    a function that reallocates a memory block using malloc and free- hot fix--2

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -49,7 +49,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 			new_p[i] = old_p[i];
 	}
 
-	if(new_size > old_size)
+	if (new_size > old_size)
 	{
 		for (i = 0; i < old_size; i++)
 			new_p[i] = old_p[i];
