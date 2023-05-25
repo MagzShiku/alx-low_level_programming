c56a9f40e74bce4b206bad4ea8638540a2348079
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-05-25 17:15:56 +0400

    a function that prints anything in a given parameter- hot fix

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -7,43 +7,41 @@
 
 void print_all(const char * const format, ...)
 {
-	unsigned int i = 0, j = 0, k;
-	char *str, fmt, *separator = "";
-	float flt;
+	unsigned int i = 0;
+	char *str, *separator = "";
 	va_list lst;
 
 	va_start(lst, format);
 
-	while (format[i] != '\0')
+	if (format)
 	{
-		fmt = format[i];
+		while (format[i])
+		{
+			switch (format[i])
+			{
+				case'c':
+					printf("%s%c", separator, va_arg(lst, int));
+					break;
+				case 'i':
+					printf("%s%d", separator, va_arg(lst, int));
+					break;
+				case 'f':
+					printf("%s%f", separator, va_arg(lst, double));
+					break;
+				case 's':
+					str = va_arg(lst, char *);
+					if (str == NULL)
+						str = "(nil)";
+					printf("%s%s", separator, str);
+					break;
+				default:
+					i++;
+					continue;
 
-	if (fmt == 'c')
-	{
-		k = va_arg(lst, int);
-		printf("%s%c", separator, k);
-	}
-	else if (fmt == 'i')
-	{
-		k = va_arg(lst, int);
-		printf("%s%d", separator, k);
-	}
-	else if (fmt == 'f')
-	{
-		flt = va_arg(lst, double);
-		printf("%s%f", separator, flt);
-	}
-	else if (fmt == 's')
-	{
-		str = va_arg(lst, char *);
-		if (str == NULL)
-			printf("nil");
-		else
-		printf("%s%s", separator, str);
-	}
-	separator = ", ";
-	i++;
-	j++;
+			}
+			separator = ", ";
+			i++;
+		}
 	}
 va_end(lst);
 printf("\n");
