cd2f5b77cd9c96a21c0e6cfba96bfc406d40ec60
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-06-02 17:15:22 +0400

    Write a function that prints You're beat! and yet, you must allow,I bore my house upon my back! before the main function is executed- hot fix[1]

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,13 +2,12 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
-
+void p_string(void) __attribute__((constructor));
 /**
  * p_string - a functin that prints a string
  * Return: the string
  */
 
-void p_string(void) __attribute__((constructor));
 void p_string(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
