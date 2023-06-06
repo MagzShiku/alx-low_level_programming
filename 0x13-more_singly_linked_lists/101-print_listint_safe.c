602bff39ac071dbeed284e18219770019df54a1e
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-06-06 09:15:34 +0400

    prints nodes of lists - hot fix-hot fix

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,6 +1,48 @@
 #include "lists.h"
 
 /**
+ * check_loop_node - checks for nodes
+ * @head: Contsins address to loop
+ * Return: 0 if the list has looped qualities
+ * Return number of special nodes
+ */
+
+size_t check_loop_node(const listint_t *head)
+{
+	const listint_t *drag;
+	const listint_t *speed;
+	size_t node_count;
+
+	node_count = 1;
+	drag = head->next;
+	speed = ((head->next)->next);
+
+	if (head == NULL || head->next == NULL)
+	{
+		return (0);
+	}
+
+	while (speed && speed->next)
+	{
+		if (drag == speed)
+		{
+			drag = head;
+			while (drag != speed->next)
+			{
+				node_count++;
+				drag = drag->next;
+				speed = speed->next;
+			}
+			return (node_count);
+		}
+		drag = drag->next;
+		speed = (speed->next)->next;
+	}
+return (0);
+
+}
+
+/**
  * print_listint_safe - a function that prints a listint_t linked list.
  * @head: contains fist address in the list
  * Return: the number of nodes in the list
