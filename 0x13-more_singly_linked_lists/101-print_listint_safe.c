febcf6134d2faf04e0554d5c29c56766094bbb1f
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-06-06 08:37:36 +0400

    prints nodes of lists - hot fix

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -12,22 +12,22 @@
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t node_count;
-	const listint_t *e;
-	const listint_t node_after;
+	const listint_t *e;  /* e represents the existing list */
+	const listint_t *node_after;
 
 	e = head;
 	node_count = 0;
 
 	while (e != NULL)
 	{
-		node_after = e->next;
-
 		printf("[%p] %d\n", (void *)e, e->n);
 		node_count++;
 
-		if (e <= node_after->next)
+		node_after = e->next;
+
+		if (node_after != NULL && node_after >= e)
 		{
-			printf("-> [%p] %d\n", (void *)node_after, nofe_after->n);
+			printf("-> [%p] %d\n", (void *)node_after, node_after->n);
 			exit(98);
 		}
 		e = node_after;
