d1b8d845609f03e2902e30db780f572db5d047a4
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-06-13 20:36:42 +0400

    elf header reader- hot fix 00

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -3,7 +3,7 @@
 #define CODE_ERR 98
 /**
  * Usage - elf_header elf_filename
- * Displayed information - (no less, no more, do 
+ * Displayed information - (no less, no more, do
  * not include trailing whitespace)
  * Magic
  * Class
@@ -14,12 +14,12 @@
  * Type
  * Entry point address
  * Format - the same as readelf -h (version 2.26.1)
- * If the file is not an ELF file, or on error, 
- * exit with status code 98 and display a 
+ * If the file is not an ELF file, or on error,
+ * exit with status code 98 and display a
  * comprehensive error message to stderr
  * You are allowed to use lseek once
  * You are allowed to use read a maximum of 2 times at runtime
- * You are allowed to have as many functions 
+ * You are allowed to have as many functions
  * as you want in your source file
  * You are allowed to use printf
  */
@@ -43,7 +43,7 @@ typedef struct {
 
 /* lets display ELF(executanle and linkable format hdr info)*/
 /**
- * show_elf_hdr_info - const ElfN_Ehdr 
+ * show_elf_hdr_info - const ElfN_Ehdr
  * ElfN_Ehdr: Parameter 1
  * elf_hdr: represents elf header structure
  * 1 display Magic number
@@ -60,7 +60,7 @@ typedef struct {
 void show_elf_hdr_info(const ElfN_Ehdr *elf_hdr)
 {
 	size_t i;
-	
+
 	printf("Magic: ");/*1*/
 	for (i = 0; i < sizeof(elf_hdr->e_ident); i++)
 	{
@@ -88,7 +88,7 @@ int main(int argc, char *argv[])
 	int file_dsc;
 	ssize_t _read;
 	ElfN_Ehdr elf_hdr;
-	
+
 	if (argc != 2)
 	{
 		fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
