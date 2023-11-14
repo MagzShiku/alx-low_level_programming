4144fa90a0528e77b55a87398b0246e33ce1738e
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-11-14 17:48:20 +0300

    sf

---
+++ 0x06-pointers_arrays_strings/pointer_size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: Always 0
+ */
+
+int main()
+{
+	int **p;
+	printf("size of a pointer: %lu bytes\n", sizeof(p));
+	return 0;
+
+}
---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * strtow - splits a string into words
+ * @str: the string to be split
+ * Return: NULL if str == NULL or str == ""
+ * NULL if function fails
+ */
+
+char **strtow(char *str)
+{
+
+}
